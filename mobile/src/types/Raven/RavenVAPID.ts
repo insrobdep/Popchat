e19727ca454d53419bb2d7fb6816df754a157a91
Aditya Patil <patiladitya781@gmail.com>
2023-12-04 17:09:05 +0530

    feat: Raven VAPID doctype

---
+++ mobile/src/types/Raven/RavenVAPID.ts
@@ -0,0 +1,17 @@
+
+export interface RavenVAPID{
+	creation: string
+	name: string
+	modified: string
+	owner: string
+	modified_by: string
+	docstatus: 0 | 1 | 2
+	parent?: string
+	parentfield?: string
+	parenttype?: string
+	idx?: number
+	/**	Public Key : Data	*/
+	public_key: string
+	/**	Private Key : Password	*/
+	private_key: string
+}
